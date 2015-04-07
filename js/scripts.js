c9b0ab14f0c0d252af6ea1e716b32fd10d8e157f
Daniel Toader & Tom Mertz <student@epicodus.com>
2015-04-07 14:33:17 -0700

    add up to 100 logic

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,27 @@
 function getWords(number) {
-  var singles = {1: "one", 2: "two", 3: "three", 4: "four", 5: "five", 6: "six", 7: "seven", 8: "eight", 9: "nine"};
-  return singles[number];
+  var ones = {1: "one", 2: "two", 3: "three", 4: "four", 5: "five", 6: "six", 7: "seven", 8: "eight", 9: "nine"};
+  var teens = {10: "ten", 11: "eleven", 12: "twelve", 13: "thirteen", 14: "fourteen", 15: "fifteen", 16: "sixteen", 17: "seventeen", 18: "eighteen", 19: "nineteen"};
+  var tens = {2: "twenty", 3: "thirty", 4: "fourty", 5: "fifty", 6: "sixty", 7: "seventy", 8: "eighty", 9: "ninety"};
+
+  var words = "";
+  while (number > 0) {
+    if (words.length > 0) {
+      words += " ";
+    }
+
+    if (number < 10) {
+      words += ones[number];
+      number = 0;
+    } else if (number < 20) {
+      words += teens[number];
+      number = 0;
+    } else if (number < 100) {
+      words += tens[parseInt(number / 10)];
+      number = number % 10;
+    }
+  }
+
+  return words;
 }
 
 
--- spec/specs.js
+++ spec/specs.js
@@ -2,4 +2,16 @@ describe('getWords', function() {
   it("returns single digit number", function() {
     expect(getWords(1)).to.equal("one");
   });
+
+  it("returns a teen number", function() {
+    expect(getWords(13)).to.equal("thirteen");
+  });
+
+  it("returns two digit number w/o singles digit", function() {
+    expect(getWords(30)).to.equal("thirty");
+  });
+
+  it("returns two digit number", function() {
+    expect(getWords(34)).to.equal("thirty four");
+  });
 });
