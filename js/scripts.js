34fc1e9b058a01202e723945719d405a29f81f53
Daniel Toader & Tom Mertz <student@epicodus.com>
2015-04-07 15:32:56 -0700

    add logic through 100 million

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,7 @@
 function getWords(number) {
   var ones = {1: "one", 2: "two", 3: "three", 4: "four", 5: "five", 6: "six", 7: "seven", 8: "eight", 9: "nine"};
   var teens = {10: "ten", 11: "eleven", 12: "twelve", 13: "thirteen", 14: "fourteen", 15: "fifteen", 16: "sixteen", 17: "seventeen", 18: "eighteen", 19: "nineteen"};
-  var tens = {2: "twenty", 3: "thirty", 4: "fourty", 5: "fifty", 6: "sixty", 7: "seventy", 8: "eighty", 9: "ninety"};
+  var tens = {2: "twenty", 3: "thirty", 4: "forty", 5: "fifty", 6: "sixty", 7: "seventy", 8: "eighty", 9: "ninety"};
 
   var words = "";
   while (number > 0) {
@@ -30,12 +30,38 @@ function getWords(number) {
     } else if (number < 100000) {
       words += tens[parseInt(number / 10000)];
       number %= 10000;
-      if (parseInt(number/1000) === 0) {
-        words += " thousand";
-      }
+      words = testThousand(words, number);
+    } else if (number < 1000000) {
+      words += ones[parseInt(number / 100000)] + " hundred";
+      number %= 100000;
+      words = testThousand(words, number);
+    } else if (number < 10000000) {
+      words += ones[parseInt(number / 1000000)] + " million";
+      number %= 1000000;
+    } else if (number < 20000000) {
+      words += teens[parseInt(number / 1000000)] + " million";
+      number %= 1000000;
+    } else if (number < 100000000) {
+      words += tens[parseInt(number / 10000000)];
+      number %= 10000000;
+      words = testMillion(words, number);
     }
   }
 
+  return words;
+}
+
+function testThousand(words, number) {
+  if (parseInt(number/1000) === 0) {
+    words += " thousand";
+  }
+  return words;
+}
+
+function testMillion(words, number) {
+  if (parseInt(number/1000000) === 0) {
+    words += " million";
+  }
   return words;
 }
 
--- spec/specs.js
+++ spec/specs.js
@@ -39,4 +39,24 @@ describe('getWords', function() {
     expect(getWords(30777)).to.equal("thirty thousand seven hundred seventy seven");
   });
 
+  it("returns hundred thousands in words", function() {
+    expect(getWords(300777)).to.equal("three hundred thousand seven hundred seventy seven");
+  });
+
+  it("returns one million in words", function() {
+    expect(getWords(1000000)).to.equal("one million");
+  });
+
+  it("returns fifteen million five hundred thirty four thousand three hundred forty three words", function() {
+    expect(getWords(15534343)).to.equal("fifteen million five hundred thirty four thousand three hundred forty three");
+  });
+
+  it("returns thirty million", function() {
+    expect(getWords(30000000)).to.equal("thirty million");
+  });
+
+  it("returns thirty three million", function() {
+    expect(getWords(33000000)).to.equal("thirty three million");
+  });
+
 });
