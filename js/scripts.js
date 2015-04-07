5e9b47f1d876d03b0b7088eca4cf3f2caf3a7f55
Daniel Toader & Tom Mertz <student@epicodus.com>
2015-04-07 15:48:17 -0700

    add error checking

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,10 @@
 function getWords(number) {
+
+  //check if the number is valid
+  if (!(number > 0 && number < 100000000)) {
+    return "Please enter a number between 1 and 100,000,000 exclusive (no commas)"
+  }
+
   var ones = {1: "one", 2: "two", 3: "three", 4: "four", 5: "five", 6: "six", 7: "seven", 8: "eight", 9: "nine"};
   var teens = {10: "ten", 11: "eleven", 12: "twelve", 13: "thirteen", 14: "fourteen", 15: "fifteen", 16: "sixteen", 17: "seventeen", 18: "eighteen", 19: "nineteen"};
   var tens = {2: "twenty", 3: "thirty", 4: "forty", 5: "fifty", 6: "sixty", 7: "seventy", 8: "eighty", 9: "ninety"};
